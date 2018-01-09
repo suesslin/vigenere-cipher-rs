855d3827e963cbafec25e7abfe217a9f3d219489
Hikaru <hkru@protonmail.com>
2018-01-09 23:38:40 +0100

    Format code

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,41 @@ fn main() {
 }
 
 fn char_to_pos(input: char) -> usize {
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().position(|c| { input == c } ).unwrap()
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .position(|c| input == c)
+        .unwrap()
 }
 
 fn encrypt(input: &str, key: &str) -> String {
     let w = input.to_string().to_uppercase();
     let k = key.to_string().to_uppercase();
 
-    let _key_alphabet_pos = k.chars().map(|c|{ char_to_pos(c) }).collect::<Vec<usize>>();
+    let _key_alphabet_pos = k.chars().map(|c| char_to_pos(c)).collect::<Vec<usize>>();
 
     // The amount of chars possible at key length
     let chunk_amount = w.len() as f32 / k.len() as f32;
 
     // Create ranges from where to where each group/chunk goes
-    let split_word: Vec<Vec<char>> = (0..chunk_amount.ceil() as usize).map(|chunk| {
-        let lowerbound = chunk * key.len();
-        let maximum = (chunk_amount.ceil() as usize) - 1;
+    let split_word: Vec<Vec<char>> = (0..chunk_amount.ceil() as usize)
+        .map(|chunk| {
+            let lowerbound = chunk * key.len();
+            let maximum = (chunk_amount.ceil() as usize) - 1;
 
-        let upperbound = match chunk == maximum {
-            // Last chunk/group upperboun
-            true => { w.len() - 1 },
-            // General chunk/group upperbound
-            false => { lowerbound + key.len() - 1 }
-        };
-        // Collect the chars
-        (lowerbound..=upperbound).map(|i| { w.chars().nth(i).unwrap()}).collect::<Vec<char>>()
-    }).collect::<Vec<Vec<char>>>();
+            let upperbound = if chunk == maximum {
+                // Last chunk/group upperbound
+                w.len() - 1
+            } else {
+                // General chunk/group upperbound
+                lowerbound + key.len() - 1
+            };
 
+            // Collect the chars
+            (lowerbound..=upperbound)
+                .map(|i| w.chars().nth(i).unwrap())
+                .collect::<Vec<char>>()
+        })
+        .collect::<Vec<Vec<char>>>();
 
     println!("{:?}", split_word);
 
