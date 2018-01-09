6bc35d5e109fd7283a81b3845a94a7cf07ea2903
Hikaru <hkru@protonmail.com>
2018-01-09 20:41:17 +0100

    Add word to key length chunks

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+// Vigenere Cipher
+#![feature(inclusive_range_syntax)]
+
+fn main() {
+    encrypt("Universum", "Earth");
+}
+
+fn char_to_pos(input: char) -> usize {
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().position(|c| { input == c } ).unwrap()
+}
+
+fn encrypt(input: &str, key: &str) -> String {
+    let w = input.to_string().to_uppercase();
+    let k = key.to_string().to_uppercase();
+
+    let mut key_alphabet_pos: Vec<usize> = Vec::new();
+
+    k.chars().for_each(|c|{
+        key_alphabet_pos.push(char_to_pos(c));
+    });
+
+    // The amount of chars possible at key length
+    let chunk_amount: f32 = w.len() as f32 / k.len() as f32;
+
+    // Create ranges from where to where each group/chunk goes
+    let split_word: Vec<Vec<char>> = (0..chunk_amount.ceil() as usize).map(|chunk| {
+        let lowerbound = chunk * key.len();
+        let upperbound = {
+            // Last chunk/group upperbound
+            if chunk == (chunk_amount.ceil() as usize) - 1 { w.len() - 1 }
+            // General chunk/group upperbound
+            else { lowerbound + key.len() - 1 }
+        };
+        // Collect the chars
+        (lowerbound..=upperbound).map(|i| { w.chars().nth(i).unwrap()}).collect::<Vec<char>>()
+    }).collect::<Vec<Vec<char>>>();
+
+
+    println!("{:?}", split_word);
+
+    "Test word".to_string()
+}
