bb4ffaa4b7b0ba532354a5aa15fcab18b104b738
Hikaru <hkru@protonmail.com>
2018-01-09 21:35:37 +0100

    Switch to match statement

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,21 @@ fn encrypt(input: &str, key: &str) -> String {
     let w = input.to_string().to_uppercase();
     let k = key.to_string().to_uppercase();
 
-    let key_alphabet_pos = k.chars().map(|c|{ char_to_pos(c) }).collect::<Vec<usize>>();
+    let _key_alphabet_pos = k.chars().map(|c|{ char_to_pos(c) }).collect::<Vec<usize>>();
 
     // The amount of chars possible at key length
-    let chunk_amount: f32 = w.len() as f32 / k.len() as f32;
+    let chunk_amount = w.len() as f32 / k.len() as f32;
 
     // Create ranges from where to where each group/chunk goes
     let split_word: Vec<Vec<char>> = (0..chunk_amount.ceil() as usize).map(|chunk| {
         let lowerbound = chunk * key.len();
-        let upperbound = {
-            // Last chunk/group upperbound
-            if chunk == (chunk_amount.ceil() as usize) - 1 { w.len() - 1 }
+        let maximum = (chunk_amount.ceil() as usize) - 1;
+
+        let upperbound = match chunk == maximum {
+            // Last chunk/group upperboun
+            true => { w.len() - 1 },
             // General chunk/group upperbound
-            else { lowerbound + key.len() - 1 }
+            false => { lowerbound + key.len() - 1 }
         };
         // Collect the chars
         (lowerbound..=upperbound).map(|i| { w.chars().nth(i).unwrap()}).collect::<Vec<char>>()
